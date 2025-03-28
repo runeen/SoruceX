16c40ef827cca79a4bcbce523deb650bd844647e
Alex Botez <a.eugen23@yahoo.com>
2025-03-28 15:51:19 +0200

    Model cu Conv2d. am scos cumva 1.7 SDR la bass dar ns exact ce convolutii aveam.

--- SourceX.py
+++ SourceX.py
@@ -78,6 +78,19 @@ def genereaza_tensor_din_stereo(tensor):
     #return output_3_axe
     return output_3_axe_nou
 
+def apply_high_pass(tensor):
+    tensor = tensor.detach().numpy()
+    cutoff = 10
+    fs = mus[0].rate
+
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(2, normal_cutoff, btype='high', analog=False)
+    y = filtfilt(b, a, tensor, 1)
+    y = torch.tensor(y.copy(), requires_grad=True, dtype=torch.float32)
+    return y
+
+
 genereaza_tensor_din_stereo(mus[0].audio)
 
 '''
@@ -99,39 +112,46 @@ nr_samples = mus[0].audio.shape[0]
 class AudioModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.seq_bands = torch.nn.Sequential(
-            torch.nn.Linear(3, 3),
-            torch.nn.Linear(3, 2),
-            torch.nn.Linear(2, 1)
-        )
-        self.seq_channels = torch.nn.Sequential(
-            torch.nn.Linear(5, 4),
-            torch.nn.Linear(4, 3),
-            torch.nn.Linear(3, 2),
-        )
-        self.seq_stems = torch.nn.Sequential(
-            torch.nn.Linear(1, 2),
-            torch.nn.Linear(2, 3),
-            torch.nn.Linear(3, 4)
-        )
+        self.encoder1 = torch.nn.Conv2d(3, 10, (20, 5), padding='same')
+        self.encoder2 = torch.nn.Conv2d(10, 15, (20, 5), padding='same')
+        #self.encoder3 = torch.nn.Conv2d(15, 20, (20, 5), padding='same')
+        #self.encoder4 = torch.nn.Conv2d(20, 60, (20, 5), padding='same')
+
+
+        #self.decoder4 = torch.nn.Conv2d(60, 20, (20, 5), padding='same')
+        #self.decoder3 = torch.nn.Conv2d(20, 15, (20, 5), padding='same')
+        self.decoder2 = torch.nn.Conv2d(15, 10, (20, 5), padding='same')
+        self.decoder1 = torch.nn.Conv2d(10, 4, (20, 5), padding='same')
 
 
     def forward(self, x):
-        #print(f'forward: {x.shape}')
-        x = self.seq_bands(x)
-        x = torch.permute(x, (0, 2 ,1))
-        x = self.seq_channels(x)
-        x = torch.permute(x, (0, 2, 1))
-        x = self.seq_stems(x)
-        x = torch.permute(x, (2, 0, 1))
-        print(x.shape)
-        return x
+        x = x.permute(2, 0, 1)
+        #saved = [x]
+
+        x = self.encoder1(x)
+        #saved.append(x)
+        x = self.encoder2(x)
+        #saved.append(x)
+        #x = self.encoder3(x)
+
+        #x = self.encoder4(x)
+
+
+        #x = self.decoder4(x)
+        #x = self.decoder3(x)
+        x = self.decoder2(x)
+        x = self.decoder1(x)
+
+        #x = x.permute()
+        return x[:, :, :2]
 
 
 
 
 dtype = torch.float32
-device = torch.device("cpu")
+device = torch.device("cuda")
+print(torch.cuda.is_available())
+torch.set_default_device("cuda")
 
 model = AudioModel()
 
@@ -142,7 +162,7 @@ criterion.requires_grad_(True)
 print(f'shape musdb {mus}')
 
 #original 2e-5
-learning_rate = 2e-2
+learning_rate = 2e-5
 #original era Adam aici
 optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate, momentum=0.9)
 
@@ -154,13 +174,19 @@ for t in range(0, 200):
     for song in range(len(mus)):
         x_true = torch.from_numpy(genereaza_tensor_din_stereo(mus[song].audio))
         x_true = x_true.to(torch.float32)
+        x_true = x_true.to(device = "cuda")
         #print(f'x_true.shape: {x_true.shape}')
 
         y_true = torch.from_numpy(mus[song].stems[1:, :, :])
         y_true = y_true.to(torch.float32)
+        y_true = y_true.to(device = "cuda")
         #print(f'y_true.shape: {y_true.shape}')
 
         y_pred = model(x_true)
+        print(f'y_pred shape: {y_pred.shape}')
+
+        #y_pred = apply_high_pass(y_pred)
+
         #print(f'y_pred.shape: {y_pred.shape}')
 
 
@@ -170,9 +196,10 @@ for t in range(0, 200):
         #plafoneaza pe la 0.05
         print(f't- {t}, song- {song}, mse: {loss.item()}, rmse:{math.sqrt(loss.item())}')
 
-        y_pred_np = y_pred.detach().numpy()
 
-        if song % 10 == 9:
+        if song % 100 == 99:
+            y_pred = y_pred.to(device="cpu")
+            y_pred_np = y_pred.detach().numpy()
             estimates = {
                 'drums': y_pred_np[0, :, :],
                 'bass': y_pred_np[1, :, :],
